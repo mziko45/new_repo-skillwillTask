4c85e6f1141ea164adc028cff4d36d5f423e5ffc
mziko45 <116023770+mziko45@users.noreply.github.com>
2022-11-06 21:54:36 +0400

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "მაქსიმალური ციფრის პოვნა";
+    
+    int   numberOne, numberTwo, numberThree;
+    cout << "მიუთითე  პირველი ციფრი"   <<endl;
+    cin >> numberOne;
+    cout << "მიუთითე მეორე რიცხვი"  <<endl;
+    cin >> numberTwo;
+    cout << "მიუთითე მესამე ციფრი"  <<endl;
+    cin >> numberThree;
+    
+    if (numberOne > numberTwo && numberOne > numberThree ) {
+        cout << " numberOne არის მაქსიმალური ციფრიი "   <<endl;
+    } else if ( numberTwo > numberOne && numberTwo > numberThree ) {
+        cout << " numberTwo არის მაქსიმალური ციფრი " <<endl;
+    }else if (numberThree > numberOne && numberThree > numberTwo) {
+        cout << " numberThree არის მაქსიმალური ციფრი "   <<endl;
+    } else if ( numberOne == numberTwo && numberTwo == numberThree) {
+        cout << " numberOne, numberTwo, numberThree ციფრები ტოლია "  <<endl;
+    }else {
+        cout << " numberThree არის მაქსიმალური ციფრი "  <<endl;
+    }
+    
+
+    return 0;
+}
