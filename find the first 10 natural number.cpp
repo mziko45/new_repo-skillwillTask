aa4302f6da81c3ffe51cc0db11897ec3adbbf0dc
mziko45 <116023770+mziko45@users.noreply.github.com>
2022-11-09 22:51:17 +0400

    Add files via upload

---
+++ find the first 10 natural number.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cout << " ვიპოვოთ პირველი 10 ნატულარული რიცხვი"<<endl;
+    cout << "------------------------------------" <<endl;
+    cout << "პირველი ნატურალური რიცხვებია:" << endl;
+     
+    for( i=1 ; i <= 10; i ++ ) 
+    {
+        cout <<i  <<endl;
+    }
+
+    return 0;
+}
